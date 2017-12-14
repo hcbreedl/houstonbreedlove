4392b19232ecc13bf62873bf661e7ec4663e0eab
Houston Breedlove <houston@Houstons-MacBook-Pro-2.local>
2017-12-14 18:59:07 -0500

    code cleanup

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,1 @@
-console.log("Hello");
-
-// $(document).ready(function() {
-//   $('.card-content.about').hide();
-//   $('.indicator').hide();
-// })
-//
-// $('.tab').on('click', function() {
-//   $('.card-content').show();
-//   $('.indicator').show();
-// })
+console.log("Ready");
