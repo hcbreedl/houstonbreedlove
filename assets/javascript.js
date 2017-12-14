30f395ec4fed9bfd3a4b91da27d00a65fa10f5dc
Houston Breedlove <houston@Houstons-MacBook-Pro-2.local>
2017-12-13 22:47:33 -0500

    added copy and adjusted styles

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+
+$(document).ready(function() {
+  $('.card-content.about').hide();
+  $('.indicator').hide();
+})
+
+$('.tab').on('click', function() {
+  $('.card-content').show();
+  $('.indicator').show();
+})
