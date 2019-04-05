bef5afd5253b6f1f15cf8a09e32dc73fea8bdd4a
hcbreedl <houstoncbreedlove@gmail.com>
2019-04-05 12:58:55 -0400

    changed to react app

---
+++ build/precache-manifest.f5084b6e8a9cd58f5083bba53e4e1ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdc4341c2fc169116fa8",
+    "url": "/static/css/main.377e64a8.chunk.css"
+  },
+  {
+    "revision": "cdc4341c2fc169116fa8",
+    "url": "/static/js/main.f8e9f63c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4892aaa87fa86d59de2ef2e9b71e5224",
+    "url": "/index.html"
+  }
+];
